42ecce78a4c5b9e747cfbf6fb38c7ee5facfb959
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2021-01-16 15:21:02 -0300

    Agrego las tablas kahoot, question y answer

--- Docker/sql/tables.sql
+++ Docker/sql/tables.sql
@@ -1,3 +1,26 @@
-create table users (
-    name varchar(30)
+CREATE TABLE kahoot (
+    id BIGINT NOT NULL PRIMARY KEY,
+    name VARCHAR(255)
 );
+
+CREATE TABLE question (
+    id BIGINT NOT NULL PRIMARY KEY,
+    question VARCHAR(255),
+    description VARCHAR(255),
+    kahoot_id BIGINT NOT NULL,
+    CONSTRAINT fk_question_kahoot
+        foreign key (kahoot_id)
+            REFERENCES kahoot(id)
+);
+
+CREATE TABLE answer (
+    id BIGINT NOT NULL,
+    answer_id BIGINT NOT NULL,
+    description VARCHAR(255),
+    id_question BIGINT NOT NULL,
+    is_true BOOLEAN,
+    PRIMARY KEY(id, answer_id),
+    CONSTRAINT fk_answer_question
+        foreign key (id_question)
+            REFERENCES question(id)
+)
