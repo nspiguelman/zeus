266b9d6dc65e1173f45fc4ddd3caff567211f373
Pedro <nahuelgalvan@gmail.com>
2021-01-19 20:03:05 -0300

    endpoint login, start y ws. Conexion ok con el cliente

--- main.go
+++ main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"github.com/nspiguelman/zeus/controllers"
-	"github.com/nspiguelman/zeus/data"
 	"github.com/nspiguelman/zeus/rest"
-	"log"
 )
 
 func main() {
 	kahootController := controllers.NewKahootController()
 
 	// dsn := "host=full_db_postgres user=zeus_db password=password dbname=zeus port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	d := data.New()
-
-	sqlDB, _ := d.DB.DB()
-
-	if err := sqlDB.Ping(); err != nil {
-		log.Panic(err.Error())
-	}
+	//d := data.New()
+	//
+	//sqlDB, _ := d.DB.DB()
+	//
+	//if err := sqlDB.Ping(); err != nil {
+	//	log.Panic(err.Error())
+	//}
 
 	server := rest.NewServer(&kahootController)
 	server.StartServer()
--- rest/router.go
+++ rest/router.go
@@ -1,18 +1,27 @@
 package rest
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/nspiguelman/zeus/controllers"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/olahol/melody.v1"
+	"log"
+	"time"
 )
 
 type Server struct {
 	router       *gin.Engine
 	kahootController *controllers.KahootController
+	socket *melody.Melody
 }
 
 func NewServer(kahootController *controllers.KahootController) *Server {
 	router := gin.Default()
-	return &Server{router, kahootController}
+	socket := melody.New()
+	return &Server{router, kahootController, socket}
 }
 
 func (s *Server) StartServer() {
@@ -22,10 +31,73 @@ func (s *Server) StartServer() {
 		})
 
 	})
+
+	/*END POINTS*/
 	s.router.POST("/room", s.kahootController.CreateKahoot())
+
+
+	//LOGIN ; Devuelve un token al usuario.
+	s.router.POST("/room/:pin/name/:name/login", func(c *gin.Context) {
+		name := c.Param("name")
+
+		var token = GenerateToken(name)
+		c.JSON(200, gin.H{
+			"token": token,
+		})
+	}) // Login users
+
+
+	//WEB SOCKET ; DONDE SE RECIBE LAS RESPUESTAS DE LOS CLIENTES
+	s.router.GET("/room/:pin/ws", func(c *gin.Context) {
+		s.socket.HandleRequest(c.Writer, c.Request)
+		go proccesAnswer(s.socket)
+
+	})
+
+	//MANDA BROADCAST
+	s.router.GET("/room/:pin/start", func(c *gin.Context) {
+
+		go broadCastQuestion(s.socket)
+
+	})
+
 
 	s.router.Run()
 }
+
+
+func proccesAnswer(m *melody.Melody) {
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		//solo imprimo el mensaje que envia el cliente.
+		//aca deberìa ir la logica de chequear la respuesta y sumar puntaje etc.
+		fmt.Printf("%s\n", msg)
+	})
+
+}
+
+
+func broadCastQuestion(m *melody.Melody){
+	time.Sleep(2 * time.Second)
+
+	b := []byte("{question: 1 = 1 ?}")
+	m.Broadcast(b)
+
+}
+
+func GenerateToken(name string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(name), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Hash to store:", string(hash))
+
+	hasher := md5.New()
+	hasher.Write(hash)
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+
 /*
 
 s.router.GET("/room/:pin/users", s.getUsers)
