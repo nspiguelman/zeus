ff6fe58c1cbd4ae64bb5d1c83b16a893ba96096f
Pedro <nahuelgalvan@gmail.com>
2021-01-21 19:24:02 -0300

    Ends points login, ws y start

--- rest/router.go
+++ rest/router.go
@@ -98,28 +98,3 @@ func GenerateToken(name string) string {
 }
 
 
-/*
-
-s.router.GET("/room/:pin/users", s.getUsers)
-
-// CREATE Game
-s.router.POST("/room", controllers.KahootController.CreateKahoot) // Create game
-s.router.POST("/room/:pin/question") // Create question
-s.router.POST("/room/:pin/question/:id_question/answer") // Create answers
-
-// Play game
-s.router.POST("/room/:pin/login") // Login users
-s.router.POST("/room/:pin/question/:id_question/user/:user_id") // Answer question
-s.router.GET("/room/:pin/next_question") // Get next question
-s.router.GET("/room/:pin/score") // Get total score
-s.router.GET("/room/:pin/user/:user_id/score") // Get user score
-
-func (s *Server) getUsers(c *gin.Context) {
-	pin := c.Param("pin")
-	c.JSON(http.StatusOK, s.kahootController.GetUsers(pin))
-}
-
-func (s *Server) createKahoot(c *gin.Context) {
-	c.JSON(http.StatusCreated, s.kahootController.CreateNewKahootGame())
-}
- */
